6c3207729bc257c8de4f2b46a6b6deb98a603127
Michael Campos <camposm1205@gmail.com>
2022-07-28 17:16:53 -0400

    Updated Customer Table
    Added vehicle table view to display selected customers vehicle

--- src/main/java/app/AppWrapper.java
+++ src/main/java/app/AppWrapper.java
@@ -5,11 +5,13 @@ package app;
  * Implement payment system to keep track of completed work orders.
  * Implement vehicle transfer of ownership between customers
  * Implement re-sizing font system in preferences
+ * Implement vehicle table view in customer view to show vehicles owned by a selected customer
  *
  * Parse SQLite database to MySQL
  *
  * When we're in the WorkOrder workspace and if we add a new customer or vehicle, then the program should update the searches for customer and vehicle
  *
+ * Implement configurable tax rate
  */
 public class AppWrapper {
     public static void main(String[] args) {
