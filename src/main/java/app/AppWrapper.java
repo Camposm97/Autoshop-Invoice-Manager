8cbdeddf54542292905bf7d9c3876f852ba8522a
Michael Campos <camposm.dev@gmail.com>
2022-08-15 19:05:18 -0400

    Updated TODO

--- src/main/java/app/AppWrapper.java
+++ src/main/java/app/AppWrapper.java
@@ -5,6 +5,7 @@ import org.controlsfx.glyphfont.FontAwesome;
 /**
  * TODO
  *  Parse SQLite database to MySQL (2)
+ *  Implement Export and Import feature for Customers & Vehicles, Suggestion products, and Work Orders.
  */
 public class AppWrapper {
     public static void main(String[] args) {
