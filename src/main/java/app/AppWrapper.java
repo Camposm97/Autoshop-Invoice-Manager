1c76f1124e980c02723c9c7cd258095294610212
Michael Campos <camposm.dev@gmail.com>
2022-08-29 13:05:39 -0400

    Updated TODO

--- src/main/java/app/AppWrapper.java
+++ src/main/java/app/AppWrapper.java
@@ -4,6 +4,9 @@ package app;
  * TODO
  * Add customer ids to work orders
  * Add listeners to Add Customer and Add Vehicle for Customer Table
+ * Implement a listener when searching for work orders to save processing time
+ *  Causes the program to slow down with big data
+ *  Implement multi-threading?
  */
 public class AppWrapper {
     public static void main(String[] args) {
