f3bf4b38d0fe4b6d04000b2a2991bc867142f6bb
CamposmDev <camposm.dev@gmail.com>
2024-06-26 00:17:41 -0400

    chore: refactor tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
-import { nextui } from "@nextui-org/react"
+import { nextui } from '@nextui-org/react'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/renderer/index.html",
-    "./src/renderer/src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/renderer/index.html', './src/renderer/src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [nextui()],
-  darkMode: "class"
+  darkMode: 'class'
 }
-
