af0694909e95f11d1cba58beefdc1d9c6618df3b
CamposmDev <camposm.dev@gmail.com>
2024-08-16 23:44:52 -0400

    fix: update eslint settings
    Removed electron prettier kit, we can prettify later...

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', '@electron-toolkit/eslint-config-ts/recommended', '@electron-toolkit/eslint-config-prettier']
+  plugins: [
+    "react"
+  ],
+  extends: [
+    'eslint:recommended', 
+    'plugin:react/recommended', 
+    'plugin:react/jsx-runtime', 
+    '@electron-toolkit/eslint-config-ts/recommended', 
+    // '@electron-toolkit/eslint-config-prettier'
+  ]
 }
